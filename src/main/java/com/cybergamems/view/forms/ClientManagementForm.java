fcf2ac99072e753e0ebd99135e160dab1d201f9b
Nguyen Hung Cuong <cuonghungnguyentop@gmail.com>
2025-03-16 15:55:59 +0700

    Sửa lại Client Management Form

--- src/main/java/com/cybergamems/view/forms/ClientManagementForm.java
+++ src/main/java/com/cybergamems/view/forms/ClientManagementForm.java
@@ -1,11 +1,11 @@
 package com.cybergamems.view.forms;
 
-
 public class ClientManagementForm extends javax.swing.JPanel {
+
     public ClientManagementForm() {
         initComponents();
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -13,7 +13,7 @@ public class ClientManagementForm extends javax.swing.JPanel {
         clientManagementHeaderPanel = new com.cybergamems.view.panels.CustomizedBorderRadiusPanel();
         tableMenuBar1 = new com.cybergamems.view.components.TableMenuBar();
         clientManagementTablePanel = new com.cybergamems.view.panels.CustomizedBorderRadiusPanel();
-        clientManagementTable1 = new com.cybergamems.view.components.ClientManagementTable();
+        clientManagementTable2 = new com.cybergamems.view.components.ClientManagementTable();
 
         setBackground(new java.awt.Color(50, 50, 50));
 
@@ -21,33 +21,35 @@ public class ClientManagementForm extends javax.swing.JPanel {
         clientManagementHeaderPanel.setLayout(clientManagementHeaderPanelLayout);
         clientManagementHeaderPanelLayout.setHorizontalGroup(
             clientManagementHeaderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(tableMenuBar1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(tableMenuBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         clientManagementHeaderPanelLayout.setVerticalGroup(
             clientManagementHeaderPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(tableMenuBar1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(tableMenuBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
+
+        clientManagementTablePanel.setPreferredSize(new java.awt.Dimension(464, 431));
 
         javax.swing.GroupLayout clientManagementTablePanelLayout = new javax.swing.GroupLayout(clientManagementTablePanel);
         clientManagementTablePanel.setLayout(clientManagementTablePanelLayout);
         clientManagementTablePanelLayout.setHorizontalGroup(
             clientManagementTablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(clientManagementTable1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(clientManagementTable2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         clientManagementTablePanelLayout.setVerticalGroup(
             clientManagementTablePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(clientManagementTable1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(clientManagementTable2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(clientManagementTablePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(clientManagementHeaderPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(clientManagementHeaderPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(clientManagementTablePanel, javax.swing.GroupLayout.DEFAULT_SIZE, 872, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -64,7 +66,7 @@ public class ClientManagementForm extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.cybergamems.view.panels.CustomizedBorderRadiusPanel clientManagementHeaderPanel;
-    private com.cybergamems.view.components.ClientManagementTable clientManagementTable1;
+    private com.cybergamems.view.components.ClientManagementTable clientManagementTable2;
     private com.cybergamems.view.panels.CustomizedBorderRadiusPanel clientManagementTablePanel;
     private com.cybergamems.view.components.TableMenuBar tableMenuBar1;
     // End of variables declaration//GEN-END:variables
